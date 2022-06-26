2f76545987a1424926712f24690048c3f12a426a
MaximTarasov <speransky1983@gmail.com>
2022-06-26 09:55:30 +0300

    added JUnit5 examples

---
+++ src/test/java/test/JUnit5Examples.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.junit.jupiter.api.*;
+
+public class JUnit5Examples {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("== Something before ALL tests");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("= Something before each tests =");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("= Something after each tests =====");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("=========Started first test");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("==========Started first test");
+    }
+    
+    @AfterAll
+    static void afterAll() {
+        System.out.println("== Something after ALL tests");
+    }
+
+
+}
